1e23139e16a31dd2bb6fbf42e57c499102be0007
Alexdelia <alexandre.delille.57@gmail.com>
2022-01-26 22:34:12 +0100

    another list function

--- Exam_Rank_04/list.c
+++ Exam_Rank_04/list.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 22:13:18 by adelille          #+#    #+#             */
-/*   Updated: 2022/01/26 22:26:21 by adelille         ###   ########.fr       */
+/*   Updated: 2022/01/26 22:33:30 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	cmd_add_av(t_cmd *cmd, char *av)
 		fatal();
 	i = -1;
 	while (++i < cmd->len)
-		tmp[i] = cmd->av[1];
+		tmp[i] = cmd->av[i];
 	if (cmd->len > 0)
 		free(cmd->av);
 	cmd->av = tmp;
@@ -30,4 +30,25 @@ int	cmd_add_av(t_cmd *cmd, char *av)
 	cmd->av[i] = 0;
 	cmd->len++;
 	return (EXIT_SUCCESS);
+}
+
+int	cmd_add_back(t_cmd **list, char *av)
+{
+	t_cmd	*new;
+
+	new = (t_cmd *)malloc(sizeof(*new));
+	if (!new)
+		fatal();
+	new->av = NULL;
+	new->len = 0;
+	new->type = T_END;
+	new->prev = NULL;
+	new->next = NULL;
+	if (*list)
+	{
+		(*list)->next = new;
+		new->prev = *list;
+	}
+	*list = new; // not sure
+	return (cmd_add_av(new, av));
 }
