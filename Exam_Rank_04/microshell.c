8e9b754733411d43712a64297f44a49d4eccbd36
adelille <adelille@student.42.fr>
2022-01-27 15:48:57 +0100

    slowly structuring

--- Exam_Rank_04/microshell.c
+++ Exam_Rank_04/microshell.c
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 15:11:28 by adelille          #+#    #+#             */
-/*   Updated: 2022/01/27 15:37:27 by adelille         ###   ########.fr       */
+/*   Updated: 2022/01/27 15:48:44 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,24 @@ int	main(int ac, char **av, char **env)
 	int	i;
 
 	i = 0;
+	// init val
+
+	while (av[i] && av[i + 1]) // could use ac
+	{
+		// move av
+		// calc len of arg
+
+		if (strcmp(av[0], "cd") == 0)
+			exec_cd(av);
+		else if (/* ; */)
+		{
+
+		}
+		else if (/* | */)
+		{
+
+		}
+	}
+	// close fd/pipe
 	return (0);
 }
