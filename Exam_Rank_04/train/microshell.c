30435abaddfa3adc35b765e6ea3e7383be1fa4b9
Alexdelia <alexandre.delille.57@gmail.com>
2022-01-29 21:15:20 +0100

    add train

---
+++ Exam_Rank_04/train/microshell.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/29 20:48:41 by adelille          #+#    #+#             */
+/*   Updated: 2022/01/29 21:14:40 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#ifdef TEST_SH
+# define TEST		1
+#else
+# define TEST		0
+#endif
+
+static void	ft_pser(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(2, str, i);
+}
+
+static int	exec(char **av, int i, int fd, char **env)
+{
+	av[i] = NULL;
+	close(fd);
+	execve(av[0], av, env);
+	ft_pser("error: cannot execute ");
+	ft_pser(av[0]);
+	ft_pser("\n");
+	return (1);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	int	pid;
+	int	pip[2];
+	int	fd;
+	int	i;
+
+	(void)ac;
+	fd = dup(STDIN_FILENO);
+	i = 0;
+	while (av[i] && av[i + 1])
+	{
+		av = &av[i + 1];
+		i = 0;
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
+			i++;
+		if (strcmp(av[0], "cd") == 0)
+		{
+			if (i != 2)
+				ft_pser("error: cd: bad arguments\n");
+			else if (chdir(av[1]))
+			{
+				ft_pser("error: cd: cannot change directory to");
+				ft_pser(av[1]);
+				ft_pser("\n");
+			}
+		}
+		else if (av != &av[i] && (av[i] == NULL || strcmp(av[i], ";") == 0))
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				dup2(fd, STDIN_FILENO);
+				if (exec(av, i, fd, env))
+					return (1);
+			}
+			else
+			{
+				close(fd);
+				waitpid(-1, NULL, WUNTRACED);
+				fd = dup(STDIN_FILENO);
+			}
+		}
+		else if (av != &av[i] && strcmp(av[i], "|") == 0)
+		{
+			pipe(pip);
+			pid = fork();
+			if (pid == 0)
+			{
+				dup2(fd, STDIN_FILENO);
+				dup2(pip[1], STDOUT_FILENO);
+				close(pip[0]);
+				close(pip[1]);
+				if (exec(av, i, fd, env))
+					return (1);
+			}
+			else
+			{
+				close(pip[1]);
+				close(fd);
+				waitpid(-1, NULL, WUNTRACED);
+				fd = dup(pip[0]);
+				close(pip[0]);
+			}
+		}
+	}
+	close(fd);
+	if (TEST)
+		while (1);
+	return (0);
+}
