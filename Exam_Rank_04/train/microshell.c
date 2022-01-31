f0b59d5fcc3d986a0eb62d84ebe115076ac83b3a
adelille <adelille@student.42.fr>
2022-01-31 14:00:45 +0100

    recode

--- Exam_Rank_04/train/microshell.c
+++ Exam_Rank_04/train/microshell.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 12:00:33 by adelille          #+#    #+#             */
-/*   Updated: 2022/01/31 09:58:07 by adelille         ###   ########.fr       */
+/*   Updated: 2022/01/31 13:59:51 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,59 +52,54 @@ int	main(int ac, char **av, char **env)
 	fd = dup(STDIN_FILENO);
 	i = 0;
 	while (av[i] && av[i + 1])
+		i++;
+	if (strcmp(av[0], "cd"))
 	{
-		av = &av[i + 1];
-		i = 0;
-		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
-			i++;
-		if (strcmp(av[0], "cd") == 0)
+		if (i != 2)
+			ft_pser("error: cd: bad arguments\n");
+		else if (chdir(av[1]))
 		{
-			if (i != 2)
-				ft_pser("error: cd: bad arguments\n");
-			else if (chdir(av[1]))
-			{
-				ft_pser("error: cd: cannot change directory to ");
-				ft_pser(av[1]);
-				ft_pser("\n");
-			}
+			ft_pser("error: cd: cannot change directory to ");
+			ft_pser(av[1]);
+			ft_pser("\n");
 		}
-		else if (av != &av[i] && (av[i] == NULL || strcmp(av[i], ";") == 0))
+	}
+	else if (av != &av[i] && (av[i] == NULL || strcmp(av[i], ";") == 0))
+	{
+		pid = fork();
+		if (pid == 0)
 		{
-			pid = fork();
-			if (pid == 0)
-			{
-				dup2(fd, STDIN_FILENO);
-				if (exec(av, i, fd, env))
-					return (1);
-			}
-			else
-			{
-				close(fd);
-				waitpid(-1, NULL, WUNTRACED);
-				fd = dup(STDIN_FILENO);
-			}
+			dup2(fd, STDIN_FILENO);
+			if (exec(av, i, fd, env))
+				return (1);
 		}
-		else if (av != &av[i] && strcmp(av[i], "|") == 0)
+		else
 		{
-			pipe(pip);
-			pid = fork();
-			if (pid == 0)
-			{
-				dup2(fd, STDIN_FILENO);
-				dup2(pip[1], STDOUT_FILENO);
-				close(pip[0]);
-				close(pip[1]);
-				if (exec(av, i, fd, env))
-					return (1);
-			}
-			else
-			{
-				close(pip[1]);
-				close(fd);
-				waitpid(-1, NULL, WUNTRACED);
-				fd = dup(pip[0]);
-				close(pip[0]);
-			}
+			close(fd);
+			waitpid(-1, NULL, WUNTRACED);
+			fd = dup(STDIN_FILENO);
+		}
+	}
+	else if (av != &av[i] && strcmp(av[i], "|") == 0)
+	{
+		pipe(pip);
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(fd, STDIN_FILENO);
+			dup2(pip[1], STDOUT_FILENO);
+			close(pip[0]);
+			close(pip[1]);
+			if (exec(av, i, fd, env))
+				return (1);
+		}
+		else
+		{
+			close(pip[1]);
+			close(fd);
+			waitpid(-1, NULL, WUNTRACED);
+			fd = dup(pip[0]);
+			close(pip[0]);
 		}
 	}
 	close(fd);
